d90643aefc9c1aa403a401706b9147fe3e4c422b
Julia <107132149+YulkaMagomedova@users.noreply.github.com>
2022-10-12 14:45:52 +0300

    Update max_intellect.py

--- max_intellect.py
+++ max_intellect.py
@@ -1,26 +1,39 @@
 # https://github.com/netology-code/py-homeworks-basic/tree/master/9.http.requests - задание 1
-
+# Программа написана для поиска самого умного супер героя из всего списка героев
+# (чтобы это сделать, надо закомментировать строку 6 и раскомментировать строку 7),
+# либо выбора самого умного героя из списка.
 import requests
 
+
+def update_dict(in_line, in_heroes_dict):
+    if int(in_line['powerstats']['intelligence']) > int(list(in_heroes_dict.values())[0]):
+        in_heroes_dict = {in_line['name']: in_line['powerstats']['intelligence']}
+    elif int(in_line['powerstats']['intelligence']) == int(list(in_heroes_dict.values())[0]):
+        in_heroes_dict.update({in_line['name']: in_line['powerstats']['intelligence']})
+    return in_heroes_dict
+
+
 heroes_list = ['Hulk', 'Captain America', 'Thanos']
-# heroes_list = None
+# heroes_list = []
 req = requests.get(f'https://cdn.jsdelivr.net/gh/akabab/superhero-api@0.3.0/api/all.json')
-tmp = {'None': 0}
-
-for line in req.json():
-    if heroes_list is not None:
-        if line['name'] in heroes_list:
-            if int(line['powerstats']['intelligence']) > int(list(tmp.values())[0]):
-                tmp = {line['name']: line['powerstats']['intelligence']}
-    else:
-        if int(line['powerstats']['intelligence']) > int(list(tmp.values())[0]):
-            tmp = {line['name']: line['powerstats']['intelligence']}
-        elif int(line['powerstats']['intelligence']) == int(list(tmp.values())[0]):
-            tmp.update({line['name']: line['powerstats']['intelligence']})
-
-if len(tmp) == 1:
-    print(f'Max intellect have {list(tmp.keys())[0]} with {list(tmp.values())[0]}')
+if len(heroes_list) > 0:
+    search_all = False
 else:
-    print('Max intellect have:')
-    for line in tmp:
-        print(f'{line} with {tmp[line]}')
+    search_all = True
+if req.status_code == 200:
+    heroes_dict = {'None': 0}
+    for line in req.json():
+        if search_all is False:
+            if line['name'] in heroes_list and len(heroes_list) != 0:
+                heroes_dict = update_dict(line, heroes_dict)
+                heroes_list.pop(heroes_list.index(line['name']))
+        else:
+            heroes_dict = update_dict(line, heroes_dict)
+    if len(heroes_dict) == 1:
+        print(f'Max intellect have {list(heroes_dict.keys())[0]} with {list(heroes_dict.values())[0]}')
+    else:
+        print('Max intellect have:')
+        for line in heroes_dict:
+            print(f'{line} with {heroes_dict[line]}')
+else:
+    print('Error: something goes wrong!')
